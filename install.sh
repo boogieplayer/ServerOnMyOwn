4a4f6514807a1501f14f8436f85285b8d1d15e6a
boogieplayer <mathieu.pasquini@gmail.com>
2018-03-17 18:21:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 ##
 
 ## CONSTANTES ##
+# COLORS
+
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 CYAN='\033[0;36m'
 END='\033[0m'
+
+# GENERATE PASSWORD
+PASSWORD_SQL = $(dd bs=30 count=1 if=/dev/urandom 2>/dev/null | base64 | tr +/ 0A)
 
