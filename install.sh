e4cce1586bf3d067dcc95e7735739cfd60b3ed31
boogieplayer <mathieu.pasquini@gmail.com>
2018-03-17 19:15:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ##
 
+clear
+
 ## CONSTANTES ##
 # COLORS
-
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
@@ -13,3 +14,28 @@ END='\033[0m'
 # GENERATE PASSWORD
 PASSWORD_SQL=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
+# MISCANDELOUS
+AUTEUR='boogieplayer'
+VERSION='1.0'
+
+## PREPARATION
+# ONLY ROOT
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# PLEASE MY EGO
+echo -e """${CYAN}
+ ____   ___   ___   ____ ___ _____ ____  _        _ __   _______ ____
+| __ ) / _ \ / _ \ / ___|_ _| ____|  _ \| |      / \\ \ / / ____|  _ \
+|  _ \| | | | | | | |  _ | ||  _| | |_) | |     / _ \\ V /|  _| | |_) |
+| |_) | |_| | |_| | |_| || || |___|  __/| |___ / ___ \| | | |___|  _ <
+|____/ \___/ \___/ \____|___|_____|_|   |_____/_/   \_\_| |_____|_| \_\
+
+
+   ${YELLOW}Server On My Own - DEBIAN${END}
+   ${GREEN}POWERED BY $AUTEUR${END}
+   ${GREEN}Version : $VERSION${END}
+   ${GREEN}Enjoy !${END}
+"""
